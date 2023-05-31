a09bad9da51d94333fdefb7a5271f26e880611a3
khalid <khalidcomilla58@gmail.com>
2023-06-01 00:56:58 +0600

    increment operator used

--- Letter frequency count.js
+++ Letter frequency count.js
@@ -1,7 +1,7 @@
 let LetterFreq = (phrase) => {
     let freq = {}
     for(let letter of phrase){
-        if(letter in freq) freq[letter] += 1
+        if(letter in freq) freq[letter]++
         else                 freq[letter] = 1
     }
     
